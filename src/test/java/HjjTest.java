da28de6788dae04781804b1b9b7c566d86e42700
hjj <15910906740@163.com>
2020-02-17 18:00:28 +0800

    firstpush

--- src/test/java/HjjTest.java
+++ src/test/java/HjjTest.java
@@ -2,5 +2,6 @@ public class HjjTest {
     public static void main(String[] args){
         System.out.print("Hello world");
         System.out.print("my name is hjj");
+        System.out.print("tijiao and push");
     }
 }
